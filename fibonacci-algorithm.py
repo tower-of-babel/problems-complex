959180d32a9f3a79a753b9dd29a992186ac900fb
İlhan Subaşı <ilhan@ilhan.name>
2019-02-24 11:28:22 +0300

    Fibonacci in Python

---
+++ fibonacci-algorithm.py
@@ -0,0 +1,11 @@
+class Fibonacci:
+	def fibonacci_nth(self, n):
+		if(n == 0): return 0
+		if(n == 1): return 1
+		else: return self.fibonacci_nth(n-1) + self.fibonacci_nth(n-2)
+
+	def fibonacci_series(self, n):
+		return [self.fibonacci_nth(x) for x in range(n+1)]
+
+
+print(Fibonacci().fibonacci_series(7))
