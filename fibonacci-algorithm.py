d6ac71e06f21dfe5e247a7a1f5bd6f03948683bd
İlhan Subaşı <ilhan@ilhan.name>
2019-06-19 00:37:20 +0300

    Use only pure functions

--- fibonacci-algorithm.py
+++ fibonacci-algorithm.py
@@ -1,14 +1,9 @@
-from functools import lru_cache
+def fibonacci_nth(n):
+	if(n == 0): return 0
+	if(n == 1): return 1
+	else: return fibonacci_nth(n-1) + fibonacci_nth(n-2)
 
-class Fibonacci:
-	@lru_cache(maxsize = 1000)
-	def fibonacci_nth(self, n):
-		if(n == 0): return 0
-		if(n == 1): return 1
-		else: return self.fibonacci_nth(n-1) + self.fibonacci_nth(n-2)
+def fibonacci_series(n):
+	return [fibonacci_nth(x) for x in range(n)]
 
-	def fibonacci_series(self, n):
-		return [self.fibonacci_nth(x) for x in range(n+1)]
-
-
-# print(Fibonacci().fibonacci_series(7))
+# print(fibonacci_series(7))
